d68073cb610d52f92a474250b7ccbaba0286b6db
DivyanshKaran <karandivyansh01@gmail.com>
2024-03-08 01:13:35 +0530

    Implemented the feed whcih shows posts

--- frontend/src/ui/MainFeed.jsx
+++ frontend/src/ui/MainFeed.jsx
@@ -1,13 +1,35 @@
 import styled from "styled-components";
+import Post from "./Post";
+import foodWithImg from "../data/JSON/foodWithImg.json";
+
+let foods = foodWithImg.slice(1, 30);
 
 const StyledMainFeed = styled.div`
   /* background-color: #f87171; */
   border-radius: 1rem;
   text-align: center;
+  display: flex;
+  flex-direction: column;
+  gap: 2.8rem;
+  padding: 0rem 3rem;
 `;
 
 function MainFeed() {
-  return <StyledMainFeed>Main Feed</StyledMainFeed>;
+  // console.log(foodWithImg);
+  // console.log(foodWithImg[34].imageURL[0]);
+  return (
+    <StyledMainFeed>
+      {foods.map((food) => (
+        <Post
+          key={food.id}
+          name={food.food_name}
+          location={"VitBhopal"}
+          imageURL={`src/data/images/images/${food?.imageURL?.at(0) || ""}`}
+          // imageURL={`src/data/images/shirley/${idx + 1}.jpg`}
+        />
+      ))}
+    </StyledMainFeed>
+  );
 }
 
 export default MainFeed;
