707b54c5b9e1a9228d6cbe6ec991907eb9c04946
Janmeyjai Mishra <mishrajanmeyjai100@gmail.com>
2024-03-11 16:33:13 +0530

    Initialised signup page

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,5 +1,57 @@
+import StyledInput from "../ui/Input";
+import StyledForm from "../ui/Form";
+import Button from "../ui/Button";
+import Row from "../ui/Row";
+
 function Signup() {
-  return <div>Sign Up</div>;
+  return (
+    <Row>
+      <div>
+        <h1>CookBook🍴</h1>
+        <p>CookBook helps you share and post your favorite delicacies!</p>
+      </div>
+      <div>
+        <StyledForm method="POST" action="/">
+          <StyledInput
+            name="email"
+            id="email"
+            type="text"
+            placeholder="Enter email or phone..."
+          ></StyledInput>
+          <Row>
+            <StyledInput
+              name="firstname"
+              id="firstname"
+              type="text"
+              placeholder="First name"
+            ></StyledInput>
+            <StyledInput
+              name="lastname"
+              id="lastname"
+              type="text"
+              placeholder="Last name"
+            ></StyledInput>
+          </Row>
+          <StyledInput
+            name="password"
+            id="password"
+            type="password"
+            placeholder="Enter password"
+          ></StyledInput>
+          <StyledInput
+            name="confirmpassword"
+            id="confirmpassword"
+            type="password"
+            placeholder="Confirm password"
+          ></StyledInput>
+          <Row type="vertical">
+            <Button>Sign Up</Button>
+            {/* <Button type="button">Forgot password?</Button> */}
+          </Row>
+        </StyledForm>
+      </div>
+    </Row>
+  );
 }
 
 export default Signup;
