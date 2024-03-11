a4567f3056d7c2abe2a1adfe51baa50c3a0ff05b
Ritvik Anand <128292481+RitvikAnand583@users.noreply.github.com>
2024-03-11 11:39:15 +0530

    file uploader add

--- backend/routers/user.router.js
+++ backend/routers/user.router.js
@@ -1,10 +1,24 @@
 import { Router } from "express";
-import { userRegister } from "../controllers/user.controllers.js";
+import { userRegister,
+         userLogin, } from "../controllers/user.controllers.js";
+import {userRecipe} from "../controllers/post.controllers.js"
+import {upload} from "../middlewares/multer.middleware.js"
 
 const router = Router();
 
 
 /* GET home page. */
-router.post('/register', userRegister );
+router.post("/register", userRegister );
+
+router.route("/login").post(userLogin);
+
+router.route("/postRecipe").post(
+    upload.fields([
+        {
+            name: "foodPicture",
+            maxCount: 1
+        }
+    ]),
+    userRecipe);
 
 export default router;
