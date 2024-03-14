9d29056d1b30bd031f61e9d9b28eb3a14ae43f79
Janmeyjai Mishra <mishrajanmeyjai100@gmail.com>
2024-03-14 14:52:26 +0530

    Initialised settings page

---
+++ frontend/src/ui/List.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const List = styled.div`
+  list-style: none;
+  font-size: 2rem;
+  gap: 1rem;
+`;
+
+export default List;
---
+++ frontend/src/ui/ListItem.jsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const ListItem = styled.div`
+  padding: 2rem;
+`;
+
+export default ListItem;
