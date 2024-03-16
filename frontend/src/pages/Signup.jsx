8d8f20c37d48397b73546908c049489500e45ea1
Janmeyjai Mishra <mishrajanmeyjai100@gmail.com>
2024-03-16 15:42:19 +0530

    Styled signup page

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -2,55 +2,64 @@ import Input from "../ui/Input";
 import StyledForm from "../ui/Form";
 import Button from "../ui/Button";
 import Row from "../ui/Row";
+import Heading from "../ui/Heading";
+import ImageContainer from "../ui/ImageContainer";
+import Message from "../ui/Message";
 
 function Signup() {
   return (
-    <Row>
-      <div>
-        <h1>CookBook🍴</h1>
-        <p>CookBook helps you share and post your favorite delicacies!</p>
-      </div>
-      <div>
-        <StyledForm method="POST" action="/">
-          <Input
-            name="email"
-            id="email"
-            type="text"
-            placeholder="Enter email or phone..."
-          ></Input>
-          <Row>
-            <Input
-              name="firstname"
-              id="firstname"
-              type="text"
-              placeholder="First name"
-            ></Input>
-            <Input
-              name="lastname"
-              id="lastname"
-              type="text"
-              placeholder="Last name"
-            ></Input>
-          </Row>
-          <Input
-            name="password"
-            id="password"
-            type="password"
-            placeholder="Enter password"
-          ></Input>
-          <Input
-            name="confirmpassword"
-            id="confirmpassword"
-            type="password"
-            placeholder="Confirm password"
-          ></Input>
+    <ImageContainer>
+      <Row style={{ height: "100vh" }}>
+        <Row style={{ backgroundColor: "black", opacity: 0.9 }}>
           <Row type="vertical">
-            <Button>Sign Up</Button>
-            {/* <Button type="button">Forgot password?</Button> */}
+            <Heading style={{ color: "white" }}>CookBook🍴</Heading>
+            <Message style={{ color: "white" }}>
+              CookBook helps you share and post your favorite delicacies!
+            </Message>
           </Row>
-        </StyledForm>
-      </div>
-    </Row>
+          <div>
+            <StyledForm method="POST" action="/">
+              <Input
+                name="email"
+                id="email"
+                type="text"
+                placeholder="Enter email or phone..."
+              ></Input>
+              <Row>
+                <Input
+                  name="firstname"
+                  id="firstname"
+                  type="text"
+                  placeholder="First name"
+                ></Input>
+                <Input
+                  name="lastname"
+                  id="lastname"
+                  type="text"
+                  placeholder="Last name"
+                ></Input>
+              </Row>
+              <Input
+                name="password"
+                id="password"
+                type="password"
+                placeholder="Enter password"
+              ></Input>
+              <Input
+                name="confirmpassword"
+                id="confirmpassword"
+                type="password"
+                placeholder="Confirm password"
+              ></Input>
+              <Row type="vertical">
+                <Button>Sign Up</Button>
+                {/* <Button type="button">Forgot password?</Button> */}
+              </Row>
+            </StyledForm>
+          </div>
+        </Row>
+      </Row>
+    </ImageContainer>
   );
 }
 
---
+++ frontend/src/ui/ImageContainer.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const ImageContainer = styled.div`
+  background-image: url("../../public/delicious-pizza-pepperoni-pizza-wallpaper-thumb.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+
+  /* width: 100vw; */
+  height: 100%;
+  /* aspect-ratio: inherit; */
+`;
+
+export default ImageContainer;
