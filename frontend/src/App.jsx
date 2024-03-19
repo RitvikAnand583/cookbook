20049a57522028bff2a69e214ca9a27fda0f1785
Janmeyjai Mishra <mishrajanmeyjai100@gmail.com>
2024-03-19 05:56:53 +0530

    Added query client and devtools

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,8 @@
 // import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import Feed from "./pages/Feed";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
@@ -19,40 +22,51 @@ import Followers from "./features/settings/Followers";
 import ProtectedRoute from "./ui/ProtectedRoute";
 import PageNotFound from "./pages/PageNotFound";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+    },
+  },
+});
+
 function App() {
   // console.log(x);
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          element={
-            <ProtectedRoute>
-              <AppLayout />
-            </ProtectedRoute>
-          }
-        >
-          <Route index element={<Navigate replace to="feed" />} />
-          <Route path="feed" element={<Feed />} />
-          <Route path="explore" element={<Explore />} />
-          <Route path="messages" element={<Messages />} />
-          <Route path="notification" element={<Notification />} />
-          <Route path="settings" element={<Settings />}>
-            <Route index element={<Navigate replace to="profile" />} />
-            <Route path="profile" element={<Profile />} />
-            <Route path="security" element={<Security />} />
-            <Route path="appearance" element={<Appearance />} />
-            <Route path="notifications" element={<Notifications />} />
-            <Route path="favorites" element={<Favorites />} />
-            <Route path="followers-following" element={<Followers />} />
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <BrowserRouter>
+        <Routes>
+          <Route
+            element={
+              <ProtectedRoute>
+                <AppLayout />
+              </ProtectedRoute>
+            }
+          >
+            <Route index element={<Navigate replace to="feed" />} />
+            <Route path="feed" element={<Feed />} />
+            <Route path="explore" element={<Explore />} />
+            <Route path="messages" element={<Messages />} />
+            <Route path="notification" element={<Notification />} />
+            <Route path="settings" element={<Settings />}>
+              <Route index element={<Navigate replace to="profile" />} />
+              <Route path="profile" element={<Profile />} />
+              <Route path="security" element={<Security />} />
+              <Route path="appearance" element={<Appearance />} />
+              <Route path="notifications" element={<Notifications />} />
+              <Route path="favorites" element={<Favorites />} />
+              <Route path="followers-following" element={<Followers />} />
+            </Route>
+            <Route path="stats" element={<Stats />} />
+            <Route path="post" element={<CreatePost />} />
           </Route>
-          <Route path="stats" element={<Stats />} />
-          <Route path="post" element={<CreatePost />} />
-        </Route>
-        <Route path="login" element={<Login />} />
-        <Route path="sign-up" element={<Signup />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
+          <Route path="login" element={<Login />} />
+          <Route path="sign-up" element={<Signup />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
