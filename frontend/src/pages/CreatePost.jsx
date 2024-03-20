889db12fac31f06807b7f8feee88c92bc9b74016
Janmeyjai Mishra <mishrajanmeyjai100@gmail.com>
2024-03-20 19:03:04 +0530

    Styled create post page

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -1,6 +1,74 @@
+import styled from "styled-components";
 import StyledForm from "../ui/Form";
 import Input from "../ui/Input";
 import Label from "../ui/Label";
+import Row from "../ui/Row";
+import Heading from "../ui/Heading";
+import Message from "../ui/Message";
+import TextArea from "../ui/Textarea";
+import Checkbox from "../ui/Checkbox";
+
+const StyledNavBar = styled.nav`
+  display: flex;
+  gap: 4rem;
+  background-color: #f4f4f5;
+  /* background-color: #fbfafb; */
+  width: fit-content;
+  padding: 0.8rem;
+  border-radius: 0.5rem;
+
+  color: "grey";
+  text-decoration: "none";
+`;
+
+const navLinkStyle = {
+  color: "grey",
+  textDecoration: "none",
+};
+
+const StyledTagGroup = styled.div`
+  padding: 1rem;
+  max-width: 60rem;
+  min-width: 30rem;
+  width: 50%;
+`;
+
+const StyledTag = styled.span`
+  padding: 0.5rem;
+`;
+
+const tags = [
+  { name: "Vegetarian", id: "veg", color: "green" },
+  { name: "Eggetarian", id: "eggveg", color: "blue" },
+  { name: "Non-Vegetarian", id: "nonveg", color: "red" },
+  { name: "Gluten-free", id: "glutenfree", color: "yellow" },
+  { name: "Vegan", id: "vegan", color: "purple" },
+  { name: "Vegan", id: "vegan", color: "purple" },
+  { name: "Vegan", id: "vegan", color: "purple" },
+  { name: "Vegan", id: "vegan", color: "purple" },
+  { name: "Vegan", id: "vegan", color: "purple" },
+  { name: "Vegan", id: "vegan", color: "purple" },
+  { name: "Vegan", id: "vegan", color: "purple" },
+  { name: "Vegan", id: "vegan", color: "purple" },
+  { name: "Vegan", id: "vegan", color: "purple" },
+  { name: "Vegan", id: "vegan", color: "purple" },
+  { name: "Vegan", id: "vegan", color: "purple" },
+  { name: "Vegan", id: "vegan", color: "purple" },
+  { name: "Vegan", id: "vegan", color: "purple" },
+  { name: "Vegan", id: "vegan", color: "purple" },
+  { name: "Vegan", id: "vegan", color: "purple" },
+  { name: "Vegan", id: "vegan", color: "purple" },
+  { name: "Vegan", id: "vegan", color: "purple" },
+  { name: "Vegan", id: "vegan", color: "purple" },
+  { name: "Vegan", id: "vegan", color: "purple" },
+  { name: "Vegan", id: "vegan", color: "purple" },
+  { name: "Vegan", id: "vegan", color: "purple" },
+  { name: "Vegan", id: "vegan", color: "purple" },
+  { name: "Vegan", id: "vegan", color: "purple" },
+  { name: "Vegan", id: "vegan", color: "purple" },
+  { name: "Vegan", id: "vegan", color: "purple" },
+  { name: "Vegan", id: "vegan", color: "purple" },
+];
 
 function CreatePost() {
   function onSubmit() {
@@ -10,54 +78,93 @@ function CreatePost() {
   }
 
   return (
-    <StyledForm action="https://httpbin.org/post" method="POST" id="form">
-      <h1>Create post here</h1>
-      <Label>Name:</Label>
-      <Input name="name" type="text" placeholder="..." />
-      <Label>Description:</Label>
-      <textarea name="description" placeholder="...write here" />
-      <Label>Upload image:</Label>
-      <Input name="avatar" type="file" accept="image/png, image,jpeg" />
-      {/* <options></options> */}
-      <div>
-        <span>
-          <input type="checkbox" name="langs" value="Vegetarian" />{" "}
-          <label htmlFor="langs_perl">Vegetarian</label>
-        </span>
-        <span>
-          <input type="checkbox" name="langs" value="Non-Vegetarian" />{" "}
-          <label htmlFor="langs_perl">Non-Vegetarian</label>
-        </span>
-        <span>
-          <input type="checkbox" name="langs" value="Gluten-free" />{" "}
-          <label htmlFor="langs_perl">Gluten-free</label>
-        </span>
-        <span>
-          <input type="checkbox" name="langs" value="Vegan" />{" "}
-          <label htmlFor="langs_perl">Vegan</label>
-        </span>
-        <span>
-          <input type="checkbox" name="langs" value="Eggetarian" />{" "}
-          <label htmlFor="langs_perl">Eggetarian</label>
-        </span>
-      </div>
-      <Label>Time Taken(in minutes):</Label>
-      <Input name="avatar" type="text" placeholder="0" />
-      <Label>Difficulty Level:</Label>
-      <div>
-        <input type="radio" name="difficultylevel" value="low" />
-        <label htmlFor="low">Low</label>
-        <input type="radio" name="difficultylevel" value="medium" />
-        <label htmlFor="medium">Medium</label>
-        <input type="radio" name="difficultylevel" value="high" />
-        <label htmlFor="high">High</label>
-      </div>
-      <Label>Recipe:</Label>
-      <textarea name="recipe" placeholder="...write here" />
-      <button onClick={() => onSubmit()} type="submit" formTarget="_blank">
-        Submit
-      </button>
-    </StyledForm>
+    <Row type="vertical" style={{ padding: "1rem" }}>
+      <Row type="vertical" variation="one-liner">
+        <Heading as="h2">Create Post here</Heading>
+        <Message style={{ fontSize: "1rem", fontWeight: 200, color: "grey" }}>
+          {" "}
+          Fill the details below to create your own post!
+        </Message>
+      </Row>
+      <hr />
+
+      <StyledForm action="https://httpbin.org/post" method="POST" id="form">
+        <Row type="vertical">
+          <Row type="vertical" variation="one-liner">
+            <Label size="1rem" color="grey">
+              Name
+            </Label>
+            <Input name="name" type="text" placeholder="..." />
+          </Row>
+          <Row type="vertical" variation="one-liner">
+            <Label size="1rem" color="grey">
+              Short description
+            </Label>
+            <TextArea name="description" placeholder="...write here" />
+          </Row>
+          <Row type="vertical" variation="one-liner">
+            <Label size="1rem" color="grey">
+              Upload avatar
+            </Label>
+            <Input name="avatar" type="file" accept="image/png, image,jpeg" />
+          </Row>
+          {/* <options></options> */}
+          <Row type="vertical" variation="one-liner">
+            <Label size="1rem" color="grey">
+              Choose tags
+            </Label>
+            <StyledTagGroup>
+              {tags.map((tag) => {
+                return (
+                  <StyledTag key={tag.id}>
+                    <input type="checkbox" name="tags" value={tag.id} />
+                    <Label
+                      htmlFor={tag.id}
+                      size="1rem"
+                      color={tag.color ? tag.color : "grey"}
+                    >
+                      {tag.name}
+                    </Label>
+                  </StyledTag>
+                );
+              })}
+            </StyledTagGroup>
+          </Row>
+          <Row type="vertical" variation="one-liner">
+            <Label size="1rem" color="grey">
+              Time Taken(in minutes):
+            </Label>
+            <Input name="avatar" type="text" placeholder="0" />
+          </Row>
+          <Row type="vertical" variation="one-liner">
+            <Label size="1rem" color="grey">
+              Difficulty Level:
+            </Label>
+            <div>
+              <input type="radio" name="difficultylevel" value="beginner" />
+              <label htmlFor="beginner">Beginner</label>
+              <input type="radio" name="difficultylevel" value="amateur" />
+              <label htmlFor="amateur">Amateur</label>
+              <input
+                type="radio"
+                name="difficultylevel"
+                value="intermmediate"
+              />
+              <label htmlFor="intermmediate">Intermmediate</label>
+              <input type="radio" name="difficultylevel" value="advanced" />
+              <label htmlFor="advanced">Advanced</label>
+              <input type="radio" name="difficultylevel" value="professional" />
+              <label htmlFor="professional">Professional</label>
+            </div>
+          </Row>
+          <Label>Recipe:</Label>
+          <textarea name="recipe" placeholder="...write here" />
+          <button onClick={() => onSubmit()} type="submit" formTarget="_blank">
+            Submit
+          </button>
+        </Row>
+      </StyledForm>
+    </Row>
   );
 }
 {
--- frontend/src/ui/Input.jsx
+++ frontend/src/ui/Input.jsx
@@ -1,16 +1,31 @@
 // /* eslint-disable react/prop-types */
 
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const Input = styled.input`
+  ${(props) => props.type === "normal" && css``}
   border: 2px solid #ecebee;
   height: 1.7rem;
   border-radius: 0.4rem;
   width: min-content;
   padding-left: 0.6rem;
+
+  /* ${(props) =>
+    props.type === "checkbox" &&
+    css`
+      height: 2.4rem;
+      width: 2.4rem;
+      outline-offset: 2px;
+      transform-origin: 0;
+      accent-color: var(--color-brand-600);
+    `} */
   /* margin: 1.5rem; */
   /* width: 20rem;
   height: 2rem; */
 `;
+
+Input.defaultProps = {
+  type: "normal",
+};
 
 export default Input;
